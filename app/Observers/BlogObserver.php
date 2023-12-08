1eb6e86cfb1c5e9ab877a4f76f85d0dc0ea4d75e
Rahma Andita Purnama <purwantiibuku@gmail.com>
2023-12-08 17:26:55 +0700

    add observer blog

---
+++ app/Observers/BlogObserver.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Blog;
+use Illuminate\Support\Facades\Storage;
+
+class BlogObserver
+{
+    /**
+     * Handle the Blog "created" event.
+     */
+    public function created(Blog $blog): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Blog "updating" event.
+     */
+    public function updating(Blog $blog): void
+    {
+	    if ($blog->isDirty('image') && ($blog->getOriginal('image') !== null)
+		    && Storage::disk('public')->exists($blog->getOriginal('image'))) {
+		    Storage::disk('public')->delete($blog->getOriginal('image'));
+	    }
+    }
+	
+	/**
+     * Handle the Blog "updated" event.
+     */
+    public function updated(Blog $blog): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Blog "deleted" event.
+     */
+    public function deleted(Blog $blog): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Blog "restored" event.
+     */
+    public function restored(Blog $blog): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Blog "force deleted" event.
+     */
+    public function forceDeleted(Blog $blog): void
+    {
+        //
+    }
+}
