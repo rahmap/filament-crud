b1956f450572e798b10a3de4fa6fac5fed0c3a3b
Rahma Andita Purnama <purwantiibuku@gmail.com>
2023-12-08 19:53:18 +0700

    category BlogsRelationManager

--- app/Filament/Resources/CategoryResource.php
+++ app/Filament/Resources/CategoryResource.php
@@ -56,7 +56,7 @@ class CategoryResource extends Resource
     public static function getRelations(): array
     {
         return [
-            //
+	        RelationManagers\BlogsRelationManager::class,
         ];
     }
     
---
+++ app/Filament/Resources/CategoryResource/RelationManagers/BlogsRelationManager.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Filament\Resources\CategoryResource\RelationManagers;
+
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Tables;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class BlogsRelationManager extends RelationManager
+{
+    protected static string $relationship = 'blogs';
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('id')
+                    ->required()
+                    ->maxLength(255),
+            ]);
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->recordTitleAttribute('id')
+            ->columns([
+	            TextColumn::make('id'),
+	            TextColumn::make('title'),
+	            TextColumn::make('content'),
+	            TextColumn::make('category.name'),
+	            ImageColumn::make('image')->circular()
+            ])
+            ->filters([
+                //
+            ])
+            ->headerActions([
+                Tables\Actions\CreateAction::make(),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ])
+            ->emptyStateActions([
+                Tables\Actions\CreateAction::make(),
+            ]);
+    }
+}
