d9e9f3e06254a8b9c08760d58a6644927bf7fecf
Rahma Andita Purnama <purwantiibuku@gmail.com>
2023-12-08 19:21:57 +0700

    ide helper

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -4,6 +4,29 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Blog
+ *
+ * @property int $id
+ * @property int|null $category_id
+ * @property string $title
+ * @property string $content
+ * @property string $image
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Models\Category|null $category
+ * @method static \Illuminate\Database\Eloquent\Builder|Blog newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Blog newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Blog query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Blog whereCategoryId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Blog whereContent($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Blog whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Blog whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Blog whereImage($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Blog whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Blog whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Blog extends Model
 {
 	protected $fillable = [
