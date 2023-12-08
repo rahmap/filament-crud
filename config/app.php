8fb421cf72e75a4be69774a09a61350975f6f9e9
Rahma Andita Purnama <purwantiibuku@gmail.com>
2023-12-08 19:23:49 +0700

    debugging bar

--- config/app.php
+++ config/app.php
@@ -170,6 +170,7 @@ return [
         App\Providers\Filament\AdminPanelProvider::class,
         App\Providers\RouteServiceProvider::class,
 	    Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+	    Barryvdh\Debugbar\ServiceProvider::class,
     ])->toArray(),
 
     /*
